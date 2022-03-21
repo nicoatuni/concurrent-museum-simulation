2a7da03223f2fc36654ed3c92c9c4da71320fde6
Nico Dinata <nicodnt@icloud.com>
2022-03-21 23:26:59 +1100

    Add initial implementation of the Location/Room monitor

---
+++ Room.java
@@ -0,0 +1,11 @@
+/**
+ * TODO: add top-level docs
+ * 
+ * @author Nico Dinata (770318)
+ */
+public class Room extends Location {
+    // constructor for creating a new Room of the given index
+    public Room(int index) {
+        super(index);
+    }
+}
