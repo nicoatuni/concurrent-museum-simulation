fc0958982ac47a18f1a3a51a2ad4b79b70460c7b
Nico Dinata <nicodnt@icloud.com>
2022-03-22 00:36:09 +1100

    Use Location as interface for Rooms instead

--- Location.java
+++ Location.java
@@ -1,63 +1,10 @@
 /**
  * TODO: add top-level docs
- * Top-level location type that the foyer and rooms extend.
- * Implements the "monitor" construct by hiding its occupied status and exposing
- * only synchronised methods.
- * 
+ *
  * @author Nico Dinata (770318)
  */
-public class Location {
-    /** Index of the Location — `null` if this Location is the Foyer. */
-    private Integer index;
+public interface Location {
+    public void escortInto(Group group) throws InterruptedException;
 
-    /** The Group currently occupying this Location (can be null). */
-    private Group occupantGroup;
-
-    // constructor for creating and initialising a new empty Location
-    public Location(Integer index) {
-        this.index = index;
-        this.occupantGroup = null;
-    }
-
-    /**
-     * TODO
-     * 
-     * @param group
-     * @throws InterruptedException
-     */
-    public synchronized void enter(Group group) throws InterruptedException {
-        while (this.occupantGroup != null) {
-            wait();
-        }
-        this.occupantGroup = group;
-        System.out.println(group + " enters " + this.roomName());
-        notifyAll();
-    }
-
-    /**
-     * TODO
-     * 
-     * @return
-     * @throws InterruptedException
-     */
-    public synchronized Group leave() throws InterruptedException {
-        while (this.occupantGroup == null) {
-            wait();
-        }
-        Group leavingGroup = this.occupantGroup;
-        this.occupantGroup = null;
-        System.out.println(leavingGroup + " leaves " + this.roomName());
-        notifyAll();
-        return leavingGroup;
-    }
-
-    /** Returns whether this location is currently occupied by a Group. */
-    public synchronized boolean hasOccupant() {
-        return this.occupantGroup != null;
-    }
-
-    /** Returns the string representation of this Location's name. */
-    private String roomName() {
-        return (this.index == null) ? "the foyer" : ("room " + this.index);
-    }
+    public Group escortOutOf() throws InterruptedException;
 }
--- Room.java
+++ Room.java
@@ -3,9 +3,38 @@
  * 
  * @author Nico Dinata (770318)
  */
-public class Room extends Location {
-    // constructor for creating a new Room of the given index
+public class Room implements Location {
+    /** Index of the room. */
+    private int index;
+
+    /** The group currently occupying this room (can be null). */
+    private Group occupantGroup;
+
+    // constructor for creating a new empty room of the given index
     public Room(int index) {
-        super(index);
+        this.index = index;
+        this.occupantGroup = null;
+    }
+
+    @Override
+    public synchronized void escortInto(Group group) throws InterruptedException {
+        while (this.occupantGroup != null) {
+            wait();
+        }
+        this.occupantGroup = group;
+        System.out.println(group + " enters room " + this.index);
+        notifyAll();
+    }
+
+    @Override
+    public synchronized Group escortOutOf() throws InterruptedException {
+        while (this.occupantGroup == null) {
+            wait();
+        }
+        Group leavingGroup = this.occupantGroup;
+        this.occupantGroup = null;
+        System.out.println(leavingGroup + " leaves room " + this.index);
+        notifyAll();
+        return leavingGroup;
     }
 }
