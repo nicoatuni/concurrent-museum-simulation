35abb0a0773682a29bd3001d8b5d309f204db57b
Nico Dinata <nicodnt@icloud.com>
2022-03-23 01:16:56 +1100

    Add docs

--- Location.java
+++ Location.java
@@ -1,10 +1,13 @@
 /**
- * TODO: add top-level docs
+ * Interface for each location in the museum, which specifies two methods that
+ * have to be implemented concerning how groups are escorted into it.
  *
  * @author Nico Dinata (770318)
  */
 public interface Location {
+    /** Escort a group into the location (intended to be thread-safe). */
     public void escortInto(Group group) throws InterruptedException;
 
+    /** Escort a group out of the location (intended to be thread-safe). */
     public Group escortOutOf() throws InterruptedException;
 }
--- Room.java
+++ Room.java
@@ -1,5 +1,7 @@
 /**
- * TODO: add top-level docs
+ * Represents each room in the museum, which is identifiable by its index and
+ * can be occupied by at most a single group. It implements the `Location`
+ * interface, which specifies how groups are escorted into and out of it.
  * 
  * @author Nico Dinata (770318)
  */
@@ -16,21 +18,36 @@ public class Room implements Location {
         this.occupantGroup = null;
     }
 
+    /**
+     * Block while the room is currently occupied, otherwise we escort the new
+     * group into it.
+     */
     @Override
-    public synchronized void escortInto(Group group) throws InterruptedException {
+    public synchronized void escortInto(Group group)
+            throws InterruptedException {
+        // if this room is currently occupied, don't escort more groups into it
         while (this.occupantGroup != null) {
             wait();
         }
+
+        // the room is empty and a new group can be escorted into it
         this.occupantGroup = group;
         System.out.println(group + " enters room " + this.index);
         notifyAll();
     }
 
+    /**
+     * Block while the room is empty, otherwise we escort the occupying group
+     * out of it.
+     */
     @Override
     public synchronized Group escortOutOf() throws InterruptedException {
+        // wait until the room is actually occupied
         while (this.occupantGroup == null) {
             wait();
         }
+
+        // the room is occupied, let's escort the group out of this room
         Group leavingGroup = this.occupantGroup;
         this.occupantGroup = null;
         System.out.println(leavingGroup + " leaves room " + this.index);
