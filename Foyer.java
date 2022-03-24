0e120b3e95c7670900d46c299d1ce46ee93851f6
Nico Dinata <nicodnt@icloud.com>
2022-03-24 20:28:28 +1100

    Add docs

--- Foyer.java
+++ Foyer.java
@@ -1,15 +1,34 @@
 /**
- * TODO: add top-level docs
+ * Represents the foyer of the museum, which handles groups entering and
+ * leaving the museum and also manages the movements of a security guard. It
+ * also implements the `Location` interface which specifies how groups can be
+ * escorted into and out of it.
  * 
  * @author Nico Dinata (770318)
  */
 public class Foyer implements Location {
+    /** Whether the security guard is inside the foyer. */
     private boolean isGuardInside;
+
+    /** The group outside the museum that is waiting to be escorted in. */
     private Group enteringGroup;
+
+    /** The group outside the museum waiting to... be "consumed". */
     private Group checkedOutGroup;
+
+    /**
+     * The group that has passed security checks and is waiting to enter the
+     * first room.
+     */
     private Group checkedInGroup;
+
+    /**
+     * The group that has just arrived from the last room and is waiting to get
+     * their bags back from the security guard.
+     */
     private Group departingGroup;
 
+    /** Constructor for creating an empty foyer with the guard inside. */
     public Foyer() {
         this.isGuardInside = true;
         this.enteringGroup = null;
@@ -18,78 +37,123 @@ public class Foyer implements Location {
         this.departingGroup = null;
     }
 
+    /**
+     * Block while the foyer is occupied (by a group) or while the guard is
+     * outside. Otherwise, the group that just left the last room enters the
+     * foyer.
+     */
     @Override
-    public synchronized void escortInto(Group group) throws InterruptedException {
-        while (this.checkedInGroup != null || this.departingGroup != null || !this.isGuardInside) {
+    public synchronized void escortInto(Group group)
+            throws InterruptedException {
+        while (this.checkedInGroup != null ||
+                this.departingGroup != null ||
+                !this.isGuardInside) {
             wait();
         }
         this.departingGroup = group;
-
         System.out.println(group + " enters the foyer");
         notifyAll();
     }
 
+    /**
+     * Block while there is no group to be escorted into the first room,
+     * otherwise we escort the checked-in group out of the foyer towards the
+     * first room.
+     */
     @Override
     public synchronized Group escortOutOf() throws InterruptedException {
         while (this.checkedInGroup == null) {
             wait();
         }
+
+        // the checked-in group leaves the foyer
         Group movingOnGroup = this.checkedInGroup;
         this.checkedInGroup = null;
-
         System.out.println(movingOnGroup + " leaves the foyer");
         notifyAll();
         return movingOnGroup;
     }
 
-    public synchronized void guardCheckInside() {
-        this.isGuardInside = true;
-        System.out.println("security guard goes in");
-        notifyAll();
-    }
-
-    public synchronized Group groupWaitingInside() {
-        return this.departingGroup;
-    }
-
-    public synchronized void guardCheckOutside() {
-        this.isGuardInside = false;
-        System.out.println("security guard goes out");
-        notifyAll();
-    }
-
-    public synchronized Group groupWaitingOutside() {
-        return this.enteringGroup;
-    }
-
-    public synchronized void escortThroughSecurity(Group group) {
-        this.isGuardInside = true;
-        this.enteringGroup = null;
-        this.checkedInGroup = group;
-        System.out.println(this.checkedInGroup + " arrives at the museum");
-        notifyAll();
-    }
-
-    public synchronized void sendOffHome(Group group) {
-        this.isGuardInside = false;
-        this.departingGroup = null;
-        this.checkedOutGroup = group;
-        notifyAll();
-    }
-
-    public synchronized void arriveAtMuseum(Group group) throws InterruptedException {
-        while (this.checkedInGroup != null || this.departingGroup != null || this.isGuardInside) {
+    /**
+     * Block while the foyer is occupied or while the guard is inside.
+     * Otherwise, consider that the group waiting outside is ready to be
+     * escorted into the museum (is ready to enter the museum).
+     */
+    public synchronized void arriveAtMuseum(Group group)
+            throws InterruptedException {
+        while (this.checkedInGroup != null ||
+                this.departingGroup != null ||
+                this.isGuardInside) {
             this.enteringGroup = group;
             wait();
         }
         this.enteringGroup = group;
     }
 
+    /**
+     * Block while there are no groups outside the museum ready to leave or
+     * while the guard is inside. Otherwise, consider that the departing group
+     * outside has been "consumed" / has departed from the museum.
+     */
     public synchronized void departFromMuseum() throws InterruptedException {
         while (this.checkedOutGroup == null || this.isGuardInside) {
             wait();
         }
+
+        // the departing group gets "consumed"
         System.out.println(this.checkedOutGroup + " departs from the museum");
         this.checkedOutGroup = null;
+    }
+
+    /** Manage the security guard to go check inside the museum. */
+    public synchronized void guardCheckInside() {
+        this.isGuardInside = true;
+        System.out.println("security guard goes in");
+        notifyAll();
+    }
+
+    /** Manage the security guard to go check outside the museum. */
+    public synchronized void guardCheckOutside() {
+        this.isGuardInside = false;
+        System.out.println("security guard goes out");
+        notifyAll();
+    }
+
+    /** Return the group waiting to be escorted to leave the museum. */
+    public synchronized Group groupWaitingInside() {
+        return this.departingGroup;
+    }
+
+    /** Return the group waiting to be escorted in to enter the museum. */
+    public synchronized Group groupWaitingOutside() {
+        return this.enteringGroup;
+    }
+
+    /**
+     * Manage the security guard to escort the group waiting outside into the
+     * museum and through the security check, leaving the group ready to be
+     * escorted into the first room.
+     */
+    public synchronized void escortThroughSecurity(Group group) {
+        this.isGuardInside = true;
+
+        // the group waiting outside is now checked in (passed security checks)
+        this.enteringGroup = null;
+        this.checkedInGroup = group;
+        System.out.println(this.checkedInGroup + " arrives at the museum");
+        notifyAll();
+    }
+
+    /**
+     * Manage the security guard to escort the departing group outside the
+     * museum, where they are ready to be "consumed" out of the system.
+     */
+    public synchronized void sendOffHome(Group group) {
+        this.isGuardInside = false;
+
+        // the group waiting inside the museum has left the building
+        this.departingGroup = null;
+        this.checkedOutGroup = group;
+        notifyAll();
     }
 }
--- SecurityGuard.java
+++ SecurityGuard.java
@@ -1,54 +1,91 @@
 /**
- * TODO: add top-level docs
+ * Represents the security guard of the museum, escorting groups into and out
+ * of the museum and helping them pass through security checks.
  * 
  * @author Nico Dinata (770318)
  */
 public class SecurityGuard extends Thread {
+    /** The foyer instance of the museum. */
     private Foyer foyer;
+
+    /** Whether the guard is currently inside the foyer. */
     private boolean isInsideFoyer;
 
+    /**
+     * Constructor for creating the security guard who starts in the foyer.
+     */
     public SecurityGuard(Foyer foyer) {
         this.foyer = foyer;
         this.isInsideFoyer = true;
     }
 
+    /**
+     * Periodically patrols between the inside and outside of the museum,
+     * escorting any groups that it finds waiting both into and out of the
+     * museum.
+     */
     @Override
     public void run() {
         while (!isInterrupted()) {
             try {
-                // If foyer is empty, periodically goes outside for an interval
-                // to check for arriving groups. If no arriving groups, periodically
-                // goes inside for an interval to check for departing groups.
-                // sleep(Params.operatePause());
                 if (this.isInsideFoyer) {
+                    // if currently inside the foyer, check for any groups
+                    // waiting inside
                     Group departingGroup = this.foyer.groupWaitingInside();
+
+                    // if there are no waiting groups, wait for a little and
+                    // check again afterwards; otherwise, if there _is_ a
+                    // waiting group, we don't wait and instead proceed
+                    // straight away
                     if (departingGroup == null) {
                         sleep(Params.operatePause());
                         departingGroup = this.foyer.groupWaitingInside();
                     }
 
                     if (departingGroup != null) {
+                        // if there is a group waiting inside, we escort it
+                        // through security checks and towards the outside of
+                        // the museum
                         sleep(Params.SECURITY_TIME + Params.WALKING_TIME);
                         this.foyer.sendOffHome(departingGroup);
                     } else {
+                        // if there is no one inside the foyer, we walk back
+                        // outside
                         sleep(Params.WALKING_TIME);
                         this.foyer.guardCheckOutside();
                     }
+
+                    // regardless of whether we found a group waiting inside,
+                    // we are now outside
                     this.isInsideFoyer = false;
+
                 } else {
-                    Group groupWaitingOutside = this.foyer.groupWaitingOutside();
-                    if (groupWaitingOutside == null) {
+                    // if currently outside the foyer, check for any groups
+                    // waiting outside
+                    Group groupOutside = this.foyer.groupWaitingOutside();
+
+                    // similar to when we are inside the museum, if there are
+                    // no waiting groups, wait for a little and check again
+                    // afterwards; otherwise, if there _is_ a waiting group,
+                    // we don't wait and instead proceed straight away
+                    if (groupOutside == null) {
                         sleep(Params.operatePause());
-                        groupWaitingOutside = this.foyer.groupWaitingOutside();
+                        groupOutside = this.foyer.groupWaitingOutside();
                     }
 
-                    if (groupWaitingOutside != null) {
+                    if (groupOutside != null) {
+                        // if there is a group waiting outside, we escort it
+                        // into the museum and through security checks
                         sleep(Params.WALKING_TIME + Params.SECURITY_TIME);
-                        this.foyer.escortThroughSecurity(groupWaitingOutside);
+                        this.foyer.escortThroughSecurity(groupOutside);
                     } else {
+                        // if there is no one outside, we walk back in
                         sleep(Params.WALKING_TIME);
                         this.foyer.guardCheckInside();
                     }
+
+                    // regardless of whether we found a group waiting outside,
+                    // we are now back inside the foyer
                     this.isInsideFoyer = true;
                 }
             } catch (InterruptedException e) {
